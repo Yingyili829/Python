fa9b5c591eba73e76fe7ff732b4a5a89878f28be
Yingyili829 <yingyili829@gmail.com>
2022-04-11 17:16:38 -0700

    commit

--- day8_Caesar Cipher.py
+++ day8_Caesar Cipher.py
@@ -1,59 +1,30 @@
 
 list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-def encrypt(plain_text,shift_amount):
-    cipher_text = ""
-    for letter in plain_text:
-        position = list.index(letter)
-        new_position = position + shift_amount
-        new_letter = list[new_position]
-        cipher_text+= new_letter
-    print(f"The encoded test is {cipher_text}")
-
-
-
-def decrypt(plain_text,shift_amount):
-    cipher_text = ""
-    for letter in plain_text:
-        position = list.index(letter)
-        new_position = position - shift_amount
-        new_letter = list[new_position]
-        cipher_text+= new_letter
-    print(f"The decoded test is {cipher_text}")
-
-
 def caesar(start_text,shift_amount,cipher_direction):
-    cipher_text = ""
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    
     for letter in start_text:
         position = list.index(letter)
-        if direction == "encode" :
-            new_position = position + shift_amount
-            new_letter = list[new_position]
-            cipher_text+= new_letter
-            
-        elif direction == "decode":
-            new_position = position - shift_amount
-            new_letter = list[new_position]
-            cipher_text+= new_letter
-        else: 
-            break
+        new_position = position + shift_amount
+        end_text+= list[new_position]
         
-    if direction == "encode": 
-        print(f"The encoded test is {cipher_text}")
-    elif direction == "decode":
-        print(f"The decoded test is {cipher_text}")
-    else:
-        print("please type in encode or decode to execute the process")
-
-caesar(start_text=text,shift_amount=shift,cipher_direction=direction)
+    if cipher_direction == "decode" or cipher_direction == "encode":
+        print(f"The {cipher_direction} test is {end_text}") 
+    else: 
+        print("\033[31;1mPlease type in encode or decode to execute the process\033[31;0m")
+        
 
 #call the function
 # A parameter is the variable listed inside the parentheses in the function definition.
 # An argument is the value that are sent to the function when it is called.
 
+caesar(start_text=text,shift_amount=shift,cipher_direction=direction)
 
